96b2bd4b47302b914fd4ebfab59ad49f37aff7fd
Ashjilynxs <danisantos8314@gmail.com>
2026-06-22 10:14:08 +0000

    approutes

--- src/Pages/Routes/AppRoutes.jsx
+++ src/Pages/Routes/AppRoutes.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Inicial from "../Inicial/Inicial";
 import SobreNos from "../Sobre Nós/Sobre_nos";
-import Cardapio from "../Cardapio/Cardapio";
-import Contato from "../Contato/Contato";
-import Pedidos from "../Pedidos/Pedidos";
 
 function AppRoutes() {
   return (
@@ -12,9 +9,6 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Inicial />} />
         <Route path="/sobre" element={<SobreNos />} />
-        <Route path="/cardapio" element={<Cardapio />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/pedidos" element={<Pedidos />} />
       </Routes>
     </BrowserRouter>
   );
