a7a9ed70669df515b125c34c7ff590f2eac9c42f
Ashjilynxs <danisantos8314@gmail.com>
2026-06-28 19:33:50 +0000

    bootstrap

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,52 @@
-import style from './Navbar.module.css';
+import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className={style.navbar}>
-      <div className={style.logo}>Instituto Recomeçar</div>
+     <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
+      <div className="container-fluid px-5">
 
-      <ul className={style.navLinks}>
-        <li><Link to="/Inicial">Início</Link></li>
-        <li><Link to="/Sobre">Sobre nós</Link></li>
+        {/* Logo */}
+        <Link className={`navbar-brand ${styles.logo}`} to="/">
+          Instituto Recomeçar
+        </Link>
 
-      </ul>
+        {/* Botão mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <button className={style.btnLogin}>Entrar</button>
+        {/* Links */}
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav gap-3">
+            <li className="nav-item">
+              <Link className={`nav-link ${styles.link}`} to="/">
+                Início
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className={`nav-link ${styles.link}`} to="/sobre">
+                Sobre nós
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Botão */}
+        <Link to="/login" className={styles.btnEntrar}>
+          Entrar
+        </Link>
+
+      </div>
     </nav>
   );
 }
